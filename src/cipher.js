17b62de84abde3c37d8d3dc1ab5dd3be95c86cab
Flavia Uribe <flavia.uribejeldes@gmail.comgit config --global user.email flavia.uribejeldes@gmail.com>
2022-12-09 10:45:16 -0300

    Agregue una variable para que me devuelva mi código solo en mayusculas y cree una variable para sacar mi código Ascii

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,17 @@
-const cipher = {
-  // ...
-};
+//Agrego mi constante cipher que será mi alfabeto
+const cipher = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"];
+console.log(cipher);
+
+//Para que me acepte solo letras Mayúsculas crearé una función y una variable 
+function soloMayúsculas(cipher){
+  let mensaje = (cipher);
+
+/*Y luego con un if y un else para avisarle al usuario que solo acepte letras mayúsculas
+if ( mensaje == cipher ); {
+  return 
+}else {
+  return "Solo se admiten letras mayúculas"
+};*/
+}
 
 export default cipher;
