f01e76fb1ce9005e6d8240940d332a63b17defc7
Flavia Uribe <flavia.uribejeldes@gmail.comgit config --global user.email flavia.uribejeldes@gmail.com>
2022-12-14 18:00:20 -0300

    actualice mis botones de encode y decode en HTML

--- src/cipher.js
+++ src/cipher.js
@@ -1,10 +1,17 @@
 //Agrego mi constante cipher que será mi alfabeto
-const cipher = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"];
+const cipher = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U","V", "W", "X", "Y", "Z"];
 console.log(cipher);
 
-//Para que me acepte solo letras Mayúsculas crearé una función y una variable 
-function soloMayúsculas(cipher){
-  let mensaje = (cipher);
+//Para que me devuelva solo letras Mayúsculas crearé una variable 
+let mayúsculas = "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z";
+let X = mayúsculas.toUpperCase();
+console.log(X);
+
+//Creo una variable llamada string para sacar mi código Ascii, que es el que da el número en que se encuentra ubicada cada una de mis letras
+let string = "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z";
+string.charCodeAt()
+console.log(string);
+
 
 /*Y luego con un if y un else para avisarle al usuario que solo acepte letras mayúsculas
 if ( mensaje == cipher ); {
@@ -12,6 +19,6 @@ if ( mensaje == cipher ); {
 }else {
   return "Solo se admiten letras mayúculas"
 };*/
-}
+
 
 export default cipher;
