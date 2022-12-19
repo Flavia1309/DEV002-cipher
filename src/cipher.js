43ab567b5d66f33204d3fee3900da8fcb92c026d
Flavia Uribe <flavia.uribejeldes@gmail.comgit config --global user.email flavia.uribejeldes@gmail.com>
2022-12-19 13:49:26 -0300

    realice mi readme

--- src/cipher.js
+++ src/cipher.js
@@ -1,18 +1,133 @@
-//Agrego mi constante cipher que será mi alfabeto
-const cipher = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U","V", "W", "X", "Y", "Z"];
-console.log(cipher);
+/*Agrego mi a mi constante cipher una funcion con offset que es el numero de desplazamientos y string que es mi abecedario*/
+const cipher = {
+  encode: function(offset, string) {
+    if(typeof offset !== "number" || typeof string !== "string"){
+      throw new TypeError();
+    }
+    let resultado = "";
+    for (let i = 0; i < string.length; i++){
+      let encriptado = string.charCodeAt(i);
+      let cifrado;
+      if (encriptado < 65 || encriptado > 90){
+        cifrado = String.fromCharCode(encriptado);
+        resultado += cifrado;
+      } else {
+        let mensajeEncriptado = (encriptado - 65 + offset) % 26 + 65;
+        cifrado = String.fromCharCode(mensajeEncriptado);
+        resultado = resultado + cifrado;
+      }
+    }
+    return resultado;
+  },
 
-//Para que me devuelva solo letras Mayúsculas crearé una variable 
+  decode: function(offset, string) {
+    if(typeof offset !== "number" || typeof string !== "string"){
+      throw new TypeError();
+    }
+    let resultado = "";
+    for (let i = 0; i < string.length; i++) {
+      let encriptado = string.charCodeAt(i);
+      let cifrado;
+      if(encriptado < 65 || encriptado > 90){
+        cifrado = String.fromCharCode(encriptado);
+        resultado += cifrado;
+      } else {
+        let mensajeEncriptado = (encriptado - 90 - offset) % 26 + 90;
+        cifrado = String.fromCharCode(mensajeEncriptado);
+        resultado = resultado + cifrado;
+      }
+    }
+    return resultado;
+  }
+};
+
+
+export default cipher
+
+  
+  /*alfabeto = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+  encode:function(numeroPosiciones, mensaje) {
+
+    //VALIDANDO QUE EL # DE LETRAS A AVANZAR SEA DE TIPO NÚMERO Y SINO QUE ARROJE UN ERROR
+
+    if (isNaN(numeroPosiciones)) {
+      throw new TypeError("Desplazamiento", "cipher.js");
+    }
+    //VALIDANDO QUE LO QUE INGRESA SEA UNA CADENA DE TEXTO
+
+    if (typeof mensaje != "string") {
+      throw new TypeError("clave", "cipher.js");
+    }
+
+    let resultado = "";
+    let posicionAnterior = 0;
+    let posicionActual = 0;
+    /*posicionAnterior;
+    posicionActual;*/
+
+    /*const posicionesAnteriores = array.from(mensaje);
+    let largo = this.alfabeto.length;
+
+    //Garantizar que el número de letras a avanzar este dentro del arreglo
+
+    numeroPosiciones = ((numeroPosiciones % largo) + largo) % alfabeto;
+
+    //recorre la letra inicial para encontrar la posicion en que se encuentra la letra con la cual se va a codificar
+    for (let i = 0; i < posicionAnterior.length; i++) {
+      posicionActual = posicionAnterior[i];
+
+      posicionAnterior = this.alfabeto.indexOf(posicionActual);
+      if (posicionAnterior == -1) {
+        resultado = resultado + posicionActual;
+
+      } else {
+        posicionActual = (posicionAnterior + numeroPosiciones) % largo;
+      }
+      posicionAnterior = this.alfabeto[posicionActual];
+      resultado = resultado + posicionAnterior;
+    }
+    return resultado;
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Cree esta constante para que vea la longitud de mi string mensaje
+
+
+
+//let encode = (x + n) % 26;
+
+
+
+
+
+/*Para que me devuelva solo letras Mayúsculas crearé una variable 
 let mayúsculas = "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z";
-let X = mayúsculas.toUpperCase();
-console.log(X);
-
+let x = mayúsculas.toUpperCase();
+console.log(x);
+ 
 //Creo una variable llamada string para sacar mi código Ascii, que es el que da el número en que se encuentra ubicada cada una de mis letras
 let string = "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z";
 string.charCodeAt()
 console.log(string);
-
-
+ 
+ 
 /*Y luego con un if y un else para avisarle al usuario que solo acepte letras mayúsculas
 if ( mensaje == cipher ); {
   return 
@@ -21,4 +136,5 @@ if ( mensaje == cipher ); {
 };*/
 
 
-export default cipher;
+
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,84 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+//const number = document.getElementById("number");
+// const resultado = document.getElementById("resultado");
+// const submit = e => {
+//     e.preventDefault();
+//     resultado.innerHTML = '';
+// }
+//mensaje.onsubmit = submit;
+
+// Obtener el boton para encriptar
+// let btnEncriptar = document.getElementById("btnEncriptar");
+let formEncriptar= document.getElementById("Encriptar");
+formEncriptar.addEventListener("submit", (e)=>  {
+    e.preventDefault();
+    const offset = document.getElementById("offset").value
+    const mensaje = document.getElementById("mensaje").value
+    const resultado = cipher.encode(offset, mensaje)
+    console.log(resultado);
+})
+let formDesencriptar= document.getElementById("Desencriptar");
+formDesencriptar.addEventListener("submit", (e)=>  {
+    e.preventDefault();
+    const offset = document.getElementById("offset").value
+    const mensaje = document.getElementById("mensaje").value
+    const resultado = cipher.encode(offset, mensaje)
+    console.log(resultado);
+
+    if (mensaje.decode(mensaje)) {
+        alert("Este es tu cifrado");
+      } else {
+        alert("muchas gracias");
+      }
+    
+    SubmitEvent.addEventListener("click", function () { 
+})
+    
+      /*const traerCifrado = validator.encode(resultado.value) 
+      resultado.textContent = traerCifrado*/
+
+    console.log(cipher);
+    
+});
+/* agrego un sonido de click para los eventos del boton para que ejecute una acción
+// btnEncriptar.addEventListener("click", resultado);
+//Realizo la funcion para mi boton de desencriptado
+// let btnDesencriptar = document.getElementById("btnDesencriptar");
+// agrego un sonido de click pra los eventos del boton para que ejecute la acción
+// btnDesencriptar.addEventListener("click", resultado);
+
+//Realizo un string con los carácteres permitido, incluye un espacio entre frases
+// let alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// //Ahora verifico si mi letra está dentro del string con una función
+// function verificarCodigo (e) {
+//     if(e.key === 'Backspace') return;
+//     if(e.key === '') return;
+//     if(alfabeto.includes(e.key)) {
+//         return;
+//     } else {
+//         e.preventDefault();
+//     }
+// }
+// function cifrado(e) {
+// // Obtener el valor de mi cifrado y usar parseint 
+//    let key = parseInt(key.value);
+// // obtener el valor del elemento resultado
+//    resultadoFinal = resultado.value;
+//    let resultado 
+//    }
+//    let resultadoFinal;
+//    if(e.target.id == "btnEncriptar") {
+// //llamo al método dentro de la funcion cipher para encriptar
+//    resultadoFinal = cipher.endode(key, resultado);
+//    }
+//    if(e.target.id == "btndesencriptar") {
+// //llama al método dentro de cipher para desencriptar
+//    resultadoFinal = cipher.decode(key, resultado);
+//    }
+// //Muestra el valor del tes¿xto encriptado y desencriptado
+//    resultadoFinal,innerHTML = resultado;
+
+// function cleanImput(){
+//     location.reload();
+// }*/
