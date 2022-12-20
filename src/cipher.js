5025984b009a3f30a134e57d56a7bc924fc4eafa
Flavia Uribe <flavia.uribejeldes@gmail.comgit config --global user.email flavia.uribejeldes@gmail.com>
2022-12-20 18:58:31 -0300

    solucionado mi decodificado

--- src/cipher.js
+++ src/cipher.js
@@ -1,41 +1,29 @@
 /*Agrego mi a mi constante cipher una funcion con offset que es el numero de desplazamientos y string que es mi abecedario*/
 const cipher = {
   encode: function (offset, string) {
-    if (offset !== null || typeof string !== "string") {
-      //throw new TypeError("Error de parámetro");
-    }
+    if (offset !== null || typeof string !== "string");
     let resultado = "";
-
-    for (let i = 0; i < string.length; i++) {
-      let encriptar = string.charCodeAt(i);
-      if (encriptar >= 65 || encriptar <= 90) {
-        resultado += String.fromCharCode(((encriptar - 65 + ( offset)) % 26) + 65)
+  for(let i = 0; i<string.length; i++) {
+    let encriptar = string.charCodeAt(i);
+if (encriptar >= 65 || encriptar <= 90) {
+  resultado += String.fromCharCode(((encriptar - 65 + (offset)) % 26) + 65);
+}
       }
-    }
-    return resultado;
+return resultado;
+    
   },
 
-  decode: function (offset, string) {
-    if ( offset !== null || typeof string !== "string") {
-      //throw new TypeError();
+decode: function (offset, string) {
+  if (offset !== null || typeof string !== "string");
+  let resultado = "";
+  for (let i = 0; i < string.length; i++) {
+    let desencriptar = string.charCodeAt(i);
+    if (desencriptar > 65 || desencriptar < 90) {
+      resultado += String.fromCharCode(desencriptar);
     }
-    let resultado = "";
-    for (let i = 0; i < string.length; i++) {
-      let encriptar = string.charCodeAt(i);
-      let desencriptar;
-      if (desencriptar > 65 || desencriptar < 90) {
-        desencriptar = String.fromCharCode(encriptar);
-        resultado += desencriptar;
-
-      } else {
-        let mensajeEncriptado = (encriptar - 90 - offset) % 26 + 90;
-        desencriptar = String.fromCharCode(mensajeEncriptado);
-        resultado = resultado + desencriptar;
-
-      }
-    }
-    return resultado;
   }
+  return resultado;
+}
 
 };
 
